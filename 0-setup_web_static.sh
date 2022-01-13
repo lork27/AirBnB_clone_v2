9f826fd68b99c4139ed31c5a5f7ad07c04e1b877
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 13:02:38 -0400

    ADDED -y to update command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Script that installs and setup nginx web server and its structure
-apt-get update
+apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
