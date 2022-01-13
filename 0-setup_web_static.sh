90bd780d66f5fb46d2d3cb5f2d2384ee4f3dc8b8
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 12:59:05 -0400

    ADDED alias to sites-avialable default file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,14 +4,8 @@ apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+echo "Hola muy buenas!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test
 chown -R ubuntu:ubuntu /data
+sed -i '\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
