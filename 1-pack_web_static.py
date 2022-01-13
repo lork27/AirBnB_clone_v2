5753aa3406b9eefb280429fc4661c610a6f0f2d1
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 19:01:56 -0400

    fabric file that packs into tgz all web_static files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# fabric file to create tgz file
+from fabric.api import local
+from datetime import datetime
+import os.path
+
+
+def do_pack():
+    """function that creates targz file out of web_static directory
+    tar -czvf versions/web_static_<year><month><day><hour><minute><second>.tgz webstatic"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    tgzname = "versions/web_static_{}.tgz".format(time)
+    if os.path.isdir("versions") is False:
+        local("mkdir versions")
+    try:
+        local("tar -cvzf {} web_static".format(tgzname))
+        return tgzname
+    except:
+        return None
