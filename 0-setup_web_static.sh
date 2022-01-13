b803cd90c00fe0c26509ca67899a0061a393d6b3
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 13:06:56 -0400

    Fixed symlink

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hola muy buenas!" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
