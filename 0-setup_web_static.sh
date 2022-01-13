ac8dc827d4a41c0024a729d2ba22b6a3872a7467
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 13:16:06 -0400

    Added -h to chown to avoid dereferences

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hola muy buenas!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i 'i 40 \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+chown -hR ubuntu:ubuntu /data
+sed -i 'i 30 \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
