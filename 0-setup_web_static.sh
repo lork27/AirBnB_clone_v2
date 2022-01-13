f7f0818fad011abd5ec037fe2133e48495f09040
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-13 17:13:05 -0400

    fixed typo on sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ mkdir -p /data/web_static/shared/
 echo "Hola muy buenas!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -hR ubuntu:ubuntu /data
-sed -i 'i 30 \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '40i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
