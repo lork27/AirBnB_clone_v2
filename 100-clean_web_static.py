e308c09ee4a730a4123cab4559d906e3829bd162
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-16 19:27:32 -0400

    made sure param is an int

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-# fabric file to create tgz file
+"""deletes out of date files"""
 from fabric.api import run, local
-import os
-
-os.environ.hosts = ["35.196.44.201", "18.208.171.12"]
 
 
 def do_clean(number=0):
     """cleans up local machine and server from old files"""
+    number = int(number)
     delnum = number + 1
     if number == 0 or number == 1:
-        local("cd versions; ls -1tr | tail -n +2 | xargs rm -rf")
-        run("cd /data/web_static/releases; ls -1tr | tail -n +2 | xargs rm -rf")
+        local("cd versions; ls -t | tail -n +2 | xargs rm -rf")
+        run("cd /data/web_static/releases; ls -t | tail -n +2 | xargs rm -rf")
     else:
-        local("cd versions; ls -1tr | tail -n +{} | xargs rm -rf".format(delnum))
-        run("cd /data/web_static/releases; ls -1tr | tail +{} | xargs rm -rf".format(delnum))
+        local("cd versions; ls -t | tail -n +{} | xargs rm -rf".format(delnum))
+        run("cd /data/web_static/releases; ls -t | tail +{} | xargs rm -rf".format(delnum))
