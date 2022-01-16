76b3cd61dd94c71ef81961ee38879e8e2c4371eb
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-15 23:26:53 -0400

    modified function so it keeps old archives according to num passed to the function

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,9 +8,10 @@ os.environ.hosts = ["35.196.44.201", "18.208.171.12"]
 
 def do_clean(number=0):
     """cleans up local machine and server from old files"""
+    delnum = number + 1
     if number <= 1:
-        local("ls -1tr | head -n -1 | xargs rm -f versions")
-        run("ls -1tr | head -n -1 | xargs rm -f /data/web_static/releases")
+        local("ls -1tr | head -n -1 | xargs rm -rf versions")
+        run("ls -1tr | head -n -1 | xargs rm -rf /data/web_static/releases")
     else:
-        local("ls -1tr | head -n -1 | xargs rm -f versions")
-        run("ls -1tr | head -n -1 | xargs rm -f /data/web_static/releases")
+        local("ls -1tr | head -n -{} | xargs rm -rf versions".format(delnum))
+        run("ls -1tr | head -n -{} | xargs rm -rf /data/web_static/releases".format(delnum))
