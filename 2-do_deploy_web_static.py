77777799a377eac7a856ed1929cf0e63c3b0902b
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-15 22:33:25 -0400

    fabric file that deploys web static content to my server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,19 +8,20 @@ env.hosts = ["35.196.44.201", "18.208.171.12"]
 
 def do_deploy(archive_path):
     """function that creates targz file out of web_static directory"""
-    if os.path.try(archive_path):
+    if os.path.exists(archive_path):
         try:
             put(archive_path, "/tmp/")
             filename = archive_path.split("/")[1]
             filename2 = filename.split(".")[0]
             name = "/data/web_static/releases/" + filename2 + "/"
             run("mkdir -p" + name)
-            run("tar -xzf /tmp/" + filename + " -C" + name)
+            run("tar -xzf /tmp/" + filename + " -C " + name)
             run("rm /tmp/" + filename)
             run("mv " + name + "web_static/* " + name)
+            run("rm -rf" + name + "web_static")
             run("rm -rf /data/web_static/current")
             run("ln -s " + name + " /data/web_static/current")
-            prin("New version deployed!")
+            print("New version deployed!")
             return True
         except:
             return False
