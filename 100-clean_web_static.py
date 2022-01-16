a2bc7ba992cc746abb9bf7a0c339882b2b394245
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-16 19:16:08 -0400

    changed from head to tail

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,8 +10,8 @@ def do_clean(number=0):
     """cleans up local machine and server from old files"""
     delnum = number + 1
     if number == 0 or number == 1:
-        local("ls -1tr | head -n -1 | xargs rm -rf versions")
-        run("ls -1tr | head -n -1 | xargs rm -rf /data/web_static/releases")
+        local("ls -1tr | tail -n +2 | xargs rm -rf versions")
+        run("ls -1tr | tail -n +2 | xargs rm -rf /data/web_static/releases")
     else:
-        local("ls -1tr | head -n -{} | xargs rm -rf versions".format(delnum))
-        run("ls -1tr | head -n -{} | xargs rm -rf /data/web_static/releases".format(delnum))
+        local("ls -1tr | tail -n +{} | xargs rm -rf versions".format(delnum))
+        run("ls -1tr | tail +{} | xargs rm -rf /data/web_static/releases".format(delnum))
