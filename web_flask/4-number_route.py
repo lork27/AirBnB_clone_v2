c63f2a751b9f57e957ac7ffbe92f7e84de1a9d79
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-28 15:07:41 -0400

    tasks 3 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""module that starts flask dev server"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    if text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is(text=None):
+    if text:
+        text = text.replace("_", " ")
+        return "Python {}".format(text)
+    else:
+        print("here!")
+        return "Python is cool"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_num(n):
+    if (isinstance(n, int)):
+        return "{:d} is a number".format(n)
+    else:
+        return
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
