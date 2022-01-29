e3266f7d8be4279ead998df4f88c6cbdb0d1a712
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-29 19:03:44 -0400

    prints all states and their cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""module that starts flask dev server"""
+from flask import Flask, render_template, g
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_state_list():
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
