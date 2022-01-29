9d80be9c5265d96b7cefef691a4b1cb49e18f181
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-29 17:27:07 -0400

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module that starts flask dev server"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
